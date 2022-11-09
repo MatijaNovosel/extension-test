e288bfcc7553181634f5f6b748522a2dab224d43
Matija Novosel <mnovosel5@gmail.com>
2022-11-09 16:03:04 +0100

    NVM check

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+var commandExists = require("command-exists-promise");
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // The command has been defined in the package.json file
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "nvm-runner.nvmRunner",
+    async () => {
+      const nvmExists = await commandExists("nvm");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "nvm-runner" is now active!');
+      if (!nvmExists) {
+        vscode.window.showErrorMessage(
+          "To run this command you need to have NVM installed!"
+        );
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('nvm-runner.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from nvm-runner!');
-	});
+      // The code you place here will be executed every time your command is executed
+      vscode.window.showInformationMessage("Hello World from nvm-runner!");
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
