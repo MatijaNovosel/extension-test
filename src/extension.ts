ce262295c2fd16a0d7a2fb4cd8c2417fae68f727
Matija Novosel <mnovosel5@gmail.com>
2022-11-09 17:01:43 +0100

    Config file check

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,21 @@
 import * as vscode from "vscode";
-var commandExists = require("command-exists-promise");
+const commandExists = require("command-exists-promise");
+const fs = require("fs");
 
 export function activate(context: vscode.ExtensionContext) {
-  // The command has been defined in the package.json file
-  // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
     "nvm-runner.nvmRunner",
     async () => {
       const nvmExists = await commandExists("nvm");
+      const configFileExists = await fs.existsSync(".test");
+
+      console.log(configFileExists);
 
       if (!nvmExists) {
         vscode.window.showErrorMessage(
           "To run this command you need to have NVM installed!"
         );
+        return;
       }
 
       // The code you place here will be executed every time your command is executed
